86bdddebe0598cd60f5350ed542abf3f76ea198f
sanjeev gautam <sanjeevworkstation@gmail.com>
2020-10-05 11:16:50 +0530

    excluded arm64 architecture for simulator

---
+++ SGSegmentedProgressBarLibrary/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Sanjeev Gautam on 05/10/20.
+//  Copyright © 2020 SG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
